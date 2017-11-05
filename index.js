cf64b9e492e95fb0f27fdf63f7e5b7494d9a878a
dapersonmgn <dapersonmgn@gmail.com>
2017-11-04 18:12:00 -0600

    7.0. extension

--- index.js
+++ index.js
@@ -5,26 +5,48 @@
 }(this, (function (bot) { 'use strict';
     const MessageBot = bot.MessageBot;
     MessageBot.registerExtension("DaPersonMGN/worldCommands", function(ex, world){
+      window.ex = ex;
+      ex.remove = function() {
+        ex.world.removeCommand("world");
+      };
+
+      ex.uninstall = ex.remove;
 
       ex.world.addCommand("world", function(player, args){
-        var write = ex.bot.getExports("console").log; //Since these should kick the player off. I never really have to send it in game.
+        if (!player.isOwner || player.name !== "SERVER") {return}
+        var write = ex.bot.getExports("console").log || ex.bot.send; //Since these should kick the player off. I never really have to send it in game.
         switch (args.toUpperCase().split(" ")[0]) {
           case "START":
-            write("Starting World...");
-            world.start().then(function(){
-              write("World Started.");
+            world.getOverview(true).then(function({status}){
+              if (status === "startup" || status === "online") {
+                write("World is already online or being started.");
+              } else {
+                world.start().then(function(){
+                  write("World is being started...");
+                });
+              }
             });
           break;
           case "STOP":
-            write("Stopping World...");
-            world.stop().then(function(){
-              write("World Stopped.");
+            world.getOverview(true).then(function({status}){
+              if (status === "stopping" || status === "offline") {
+                write("World is already offline or being stopped.");
+              } else {
+                world.stop().then(function(){
+                  write("World is being stopped...");
+                });
+              }
             });
           break;
           case "RESTART":
-            write("Restarting World...");
-            world.restart().then(function(){
-              write("Restarted World.");
+            world.getOverview(true).then(function({status}){
+              if (status !== "online") {
+                write("World cannot be restarted until the world is online.");
+              } else {
+                world.restart().then(function(){
+                  write("World is being restarted...");
+                });
+              }
             });
           break;
         }
